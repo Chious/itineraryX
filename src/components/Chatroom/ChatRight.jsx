19e2d899e727cd66c6a8fd9628111ee83ba55386
Chiou Jia Sheng <100426464+Chious@users.noreply.github.com>
2023-12-11 11:14:45 +0800

    Add Dropzone to chatroom (#11)
    * Add Image to chatroom
    * Reset Router

--- src/components/Chatroom/ChatRight.jsx
+++ src/components/Chatroom/ChatRight.jsx
@@ -1,9 +1,19 @@
 import { Box, Stack } from "@mui/material";
+import Image from "mui-image";
 
-export default function ChatRight({ message, time }) {
+export default function ChatRight({ message, time, isImage }) {
+  const imgSrc = message;
+  const renderMessage = isImage ? (
+    <Image src={imgSrc} duration={0} />
+  ) : (
+    <p>{message}</p>
+  );
+
   return (
     <Stack sx={{ p: 1 }} spacing={1} alignItems="flex-end">
-      <Box sx={{ background: "#F7E896", p: 1, width: "70%" }}>{message}</Box>
+      <Box sx={{ background: "#F7E896", p: 1, width: "70%" }}>
+        {renderMessage}
+      </Box>
       <p style={{ width: "100px", textAlign: "end" }}>{time}</p>
     </Stack>
   );
